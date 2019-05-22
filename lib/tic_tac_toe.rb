cd8dd2a637d72bd059004a93af9dc50314ea3047
Alan Krajina <alankrajina88@gmail.com>
2019-05-22 09:28:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,8 +37,8 @@ WIN_COMBINATIONS = [
   
   
   
-  def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@board, index)
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
   end
   
   
