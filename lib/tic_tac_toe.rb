a66d64416ef8d4860e7cfc00b81d3bf7e7490c26
Alan Krajina <alankrajina88@gmail.com>
2019-05-22 09:47:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,15 +65,15 @@ WIN_COMBINATIONS = [
     input = gets.strip
     index = input_to_index(input)
     
-  if valid_move?(board, index) 
-    move(board, index, current_player(board)) 
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+      if valid_move?(board, index) 
+        move(board, index, current_player(board)) 
+       if valid_move?(board, index)
+        move(board, index)
+        display_board(board)
+        else
+        turn(board)
+      end
+    end
   end
   
   
